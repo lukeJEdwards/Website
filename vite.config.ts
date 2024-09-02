089cfee8d39fdd410d60b95fe64005d5fc040322
Luke Edwards <lukeedwards30167116@gmail.com>
2024-09-02 01:34:52 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
 import Markdown from "vite-plugin-md";
+import MarkdownItAnchor from "markdown-it-anchor";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,16 @@ export default defineConfig({
     vue({
       include: [/\.vue$/, /\.md$/], // <--
     }),
-    Markdown(),
+    Markdown({
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+      markdownItSetup(md) {
+        // add anchor links to your H[x] tags
+        md.use(MarkdownItAnchor);
+      },
+    }),
   ],
 });
